e7b33ae02220e5e1979190c420a682bf301ff624
Danny Walsh <dannywalsh@users.noreply.github.com>
2014-07-22 13:10:06 +0100

    added generator to yield nodes

--- Level24/level24.py
+++ Level24/level24.py
@@ -9,7 +9,7 @@ def getIm():
     return Image.open('resources/maze.png')
 
 def getStartPo(im = getIm()):
-    return dict(nextPo = [(im.size[0]-2, 0)], currentPo=(0,0))
+    return [dict(nextPo = (im.size[0]-2, 0), currentPo=(0, 0), redValue=(0))]
 
 def hasHitWall(position):
     return getIm().getpixel(position) == getWallP() 
@@ -29,38 +29,51 @@ def getWallP(im = getIm()):
     return im.getpixel((im.size[0]-1, 0))
 
 def addT(a,b):
-    #print "a : %r b : %r" % (a,b,)
     return tuple(map(sum,zip(a,b)))
 
-def getMoves(po, moves = ((-1,0),(0,1),(1,0),(0,-1))):
-    return ( addT(po, move) for move in moves)
+def getMoves(po, moves = [(-1,0),(0,1),(1,0),(0,-1)]):
+    return [addT(po,m) for m in moves]
     
 def getValidMoves(pos, cond = validate, Fn = getMoves):
-    return getNextPo(pos['nextPo'][0] ,[valid for valid in Fn(pos['nextPo'][0]) if cond(valid,pos['currentPo'])])
+    return [getNextPo(pos['nextPo'],v) for v in Fn(pos['nextPo']) if cond(v,pos['currentPo'])]
+
+def getRedValue(current, im = getIm()):
+    return im.getpixel(current)[0]
 
 def getNextPo(current, item):
-    return dict(nextPo = item, currentPo = current)
+    return dict(nextPo = item, currentPo = current, redValue = getRedValue(current))
 
 def transit(position, getNextValue, breakCond):
     yield position
-
-    for x in transit(getNextValue(position), getNextValue, breakCond):
+    
+    for x in transit(getNextValue(position[0]), getNextValue, breakCond): 
         yield x
+
         if breakCond(x):
+            print "\n BREAK CONDITION"
             break
 
-def getNodes(start):
-    for trans in transit(start, getValidMoves, lambda x: False ):
-        if len(trans['nextPo']) > 1:
-            yield trans    
-            for po in trans['nextPo']:
-                getNodes(po)    
-   
-def main():
-    #for i in islice(transit(getStartPo(), getValidMoves, lambda x: True if len(x['nextPo']) != 1 else False), 50):
-    #    print(i)
-    for i in islice(getNodes(getStartPo()),50):
-        print(i)
+def getNextNode(po):
+    for i in transit(po, getValidMoves, lambda x : True if len(x) > 1 else False):
+        if len(i) > 1:
+            #print "what is i %r"%(i,)
+            return i
+        elif len(i) == 0:
+            "Deadend : %r"%(po,)
+            return []
 
+def process(start):
+    yield start
+    #print "start : %r"%(start,)
+    #print "getstartnode %r"%(getNextNode(start),)
+    for nodelist in getNextNode(start):
+        #print "node : %r"%(nodelist,)
+        for n in process([nodelist]):
+            #print "n : %r"%(n,)
+            yield n
+
+def main():
+    for c, i in islice(izip(count(1),process(getStartPo())),1000):
+        print "count %d :  %r " % (c,i,)
 
 if __name__ == "__main__" : main()
