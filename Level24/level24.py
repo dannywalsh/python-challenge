60e67ce2d839a7522a6eba86427dca20010839f5
Danny Walsh <dannywalsh@users.noreply.github.com>
2014-07-25 13:54:01 +0100

    Removed recursive function for getting nodes. uses a list now

--- Level24/level24.py
+++ Level24/level24.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from itertools import *
+from pprint import pprint
 
 def debug(name,value):
     for n,v in zip(name,value):
@@ -35,6 +36,8 @@ def getMoves(po, moves = [(-1,0),(0,1),(1,0),(0,-1)]):
     return [addT(po,m) for m in moves]
     
 def getValidMoves(pos, cond = validate, Fn = getMoves):
+    if len(pos) == 0:
+        return []
     return [getNextPo(pos['nextPo'],v) for v in Fn(pos['nextPo']) if cond(v,pos['currentPo'])]
 
 def getRedValue(current, im = getIm()):
@@ -44,55 +47,49 @@ def getNextPo(current, item):
     return dict(nextPo = item, currentPo = current)
 
 def transit(position, getNextValue, breakCond):
+    #print"position : %r"%(position,)
     yield position
     
     for x in transit(getNextValue(position[0]), getNextValue, breakCond): 
         yield x
 
         if breakCond(x):
-            print "\n BREAK CONDITION"
+            #print "\n BREAK CONDITION"
             break
 
 def getNextNode(po):
-    for i in transit(po, getValidMoves, lambda x : True if len(x) > 1 else False):
+    for i in transit(po, getValidMoves, lambda x : True if len(x) != 1 else False):
+        #print"iiii : %r"%(i,)
         if len(i) > 1:
             #print "what is i %r"%(i,)
             return i
-        elif len(i) == 0:
-            #"Deadend : %r"%(po,)
-            return []
+    #print"Something wrong"
+    return []
 
-def process(start):
-    yield start
-    for nodelist in getNextNode(start):
-        for n in process([nodelist]):
-            yield n
-
-def findIndex(aList, pos):
-    for index, item in enumerate(aList):
-        if item['currentPo'] == pos[0]['currentPo']:
-            return index
-
-def showDeadEnd(aList):
-    for item in aList:
-        print"DeadEnd : %r"%(item,)
-
-def getValidNodeList(aList, lastIndex):
-    if lastIndex != None:
-        showDeadEnd(aList[lastIndex:])
-        return aList[:lastIndex]
+def checkList(aList):
+    if len(aList[-1]) == 0:
+        del aList[-1]
+        print "Deadend : %r"%(aList[-1][0])
+        del aList[-1][0]
+        return checkList(aList)
     return aList
 
 def main():
-    aList = []
-    for c, i in islice(izip(count(1),process(getStartPo())),10000):
-        i[0]['count'] = c
-        if i[0]['currentPo'] == (1,639):
-            print"END"
+    aList = [getStartPo()]
+    #print"aList : %r"%(aList,)
+    #print"aList[-1] : %r"%(aList[-1],)
+    #print"aList[-1][0] : %r"%(aList[-1][0],)
+    for i in islice(count(),50):
+        #print("aList")
+        #pprint(aList)
+        #print"Before append : %r"%(aList,)
+        aList.append(getNextNode([aList[-1][0]])) 
+        #print"Before checklist after append : %r"%(aList,)
+        aList = checkList(aList)
+        #print"After checklist : %r"%(aList,)
+        if aList[-1][0]['currentPo'] == (1,639):
             break
-        aList = getValidNodeList(aList, findIndex(aList,i))
-        aList.append(i[0])
-        #print "count %d :  %r " % (c,i,)
+
     with open('resources/save.txt','w') as f:
         for item in aList:
             f.write("%s\n"%item)
